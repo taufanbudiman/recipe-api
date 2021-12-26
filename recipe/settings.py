4f43d57de6ff767aeccbdff613112a4ccb2f0a45
Taufan Budiman <taufan.budiman@bhinneka.com>
2021-12-26 11:34:18 +0700

    initial commit

--- recipe/settings.py
+++ recipe/settings.py
@@ -25,10 +25,10 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = "django-insecure-!_6!cklb@aj5b$-7)2*##v83$%@%_%k8&%e46rfx3%_mp)hh3#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
